f52328b5d68996eb7bb316c80ca9dd5d8965b3ad
Qing <haru61410@gmail.com>
2025-08-10 14:11:16 +0800

    feat: enable update

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ if ("serviceWorker" in navigator) {
         console.log("SW registered: ", registration);
 
         // // 版本更新檢測
-        // registration.addEventListener("updatefound", () => {
-        //   console.log("新版本可用，建議重新載入頁面");
-        //   // 可以顯示提示要求用戶重新載入
-        //   if (confirm("發現新版本，是否要重新載入頁面？")) {
-        //     window.location.reload();
-        //   }
-        // });
+        registration.addEventListener("updatefound", () => {
+          console.log("新版本可用，建議重新載入頁面");
+          // 可以顯示提示要求用戶重新載入
+          if (confirm("發現新版本，是否要重新載入頁面？")) {
+            window.location.reload();
+          }
+        });
       })
       .catch((registrationError) => {
         console.log("SW registration failed: ", registrationError);
