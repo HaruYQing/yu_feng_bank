3e41f801cb9981fc84db79dfa2e1eff4be60ec4f
Qing <haru61410@gmail.com>
2025-08-10 20:58:52 +0800

    feat: optimize img loading [lazy loading]

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if ("serviceWorker" in navigator) {
       .then((registration) => {
         console.log("SW registered: ", registration);
 
-        // // 版本更新檢測
+        // 版本更新檢測
         registration.addEventListener("updatefound", () => {
           console.log("新版本可用，建議重新載入頁面");
           // 可以顯示提示要求用戶重新載入
@@ -30,6 +30,25 @@ document.getElementById("presonalId").addEventListener("input", function () {
   this.value = displayValue;
 });
 
+// 導航
 function navigate(url) {
   window.location.assign(url);
 }
+
+// 圖片載入
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll("img[data-src]");
+
+  const imageObserver = new IntersectionObserver((entries, _observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        img.src = img.dataset.src;
+        img.classList.remove("lazy");
+        imageObserver.unobserve(img);
+      }
+    });
+  });
+
+  images.forEach((img) => imageObserver.observe(img));
+});
